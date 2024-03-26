b7fc3e69dcad003a39f9c17adc502722f09b0439
MasatoKihara25 <1539.kihara@gmail.com>
2024-03-27 00:34:54 +0900

    Mapを使用。/helloをリクエストした時にJSONのデータで返すようにしました。

--- src/main/java/com/assignment6/hello/HelloController.java
+++ src/main/java/com/assignment6/hello/HelloController.java
@@ -3,10 +3,12 @@ package com.assignment6.hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello(){
-        return "hello world";
+    public Map<String, String> hello() {
+        return java.util.Map.of("hello", "hello world");
     }
 }
